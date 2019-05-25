702a93033a50acd956f83e159ed6f7d8c9cfde98
Vokivon <vokivon.p@gmail.com>
2019-05-25 14:09:08 +0300

    Косметические изменения

---
+++ Math_them_06.py
@@ -0,0 +1,38 @@
+#%%
+import numpy as np
+# get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib.pyplot as plt
+
+#%% [markdown]
+# ## Задания к теме 6
+
+#%%
+
+#%%
+A = np.array([[1, 2, 3], [4, 0, 6], [7, 8, 9]])
+B = np.array([12, 2, 1])
+X = np.linalg.solve(A, B)
+print("Решение:\n", X)
+
+#%% [markdown]
+# Решение через обратную матрицу
+# X = inv(A)⋅B
+#
+# Решение возможно только если определитель A != 0
+#%%
+dA = np.linalg.det(A)
+print("det =", dA)
+if dA != 0:
+    print("Можем найти решение!")
+    print("Обратная матрица:")
+    A1 = np.linalg.inv(A)
+    print(A1)
+    X1 = np.dot(A1, B)
+
+    print("Решение:\n", X1)
+else:
+    print("Система не имеет решения")
+
+
+
+
