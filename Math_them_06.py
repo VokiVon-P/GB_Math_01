81afc793e3fa547ca7bc69962b21bc8473058615
Vokivon <vokivon.p@gmail.com>
2019-05-25 18:46:58 +0300

    Закончил драфт 4 задания

--- Math_them_06.py
+++ Math_them_06.py
@@ -2,6 +2,8 @@
 import numpy as np
 # get_ipython().run_line_magic('matplotlib', 'inline')
 import matplotlib.pyplot as plt
+import scipy 
+import scipy.linalg as ln
 
 #%% [markdown]
 # ## Задания к теме 6
@@ -35,11 +37,66 @@ else:
 
 
 #%% [markdown]
-# #### Решение переопределенной СЛАУ
+# #### 2 - Решение переопределенной СЛАУ
 #### $x + 2y -z = 4$
 #### $3x – 4y = 7$
 #### $8x – 5y + 2z = 12$
 #### $2x – 5z = 7$
 #### $11x +4y – 7z = 15$
+
+#%%
+A = np.array([[1, 2, -1], [3, -4, 0], [8, -5, 2], [2, -5, 0], [11, 4, -7]])
+B = np.array([4, 7, 12, 7, 15])
+res = np.linalg.lstsq(A, B)
+print(res)
+print("Псевдорешение:\n",res[0])
+print("Сумма квадратов отклонений(невязок): = ", res[1][0])
+print("Ранк = ",res[2])
+
+#%% [markdown]
+# #### 3 - Решение  квадратной матрицы
+
+#%%
+A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+B = np.array([[12, 2, 1]])
+C = np.concatenate((A,B.T), axis=1)
+print("A:\n",A)
+print("C:\n",C)
+print("Rank A = ", np.linalg.matrix_rank(A, 0.0001))
+print("Rank C = ", np.linalg.matrix_rank(C, 0.0001))
+print("Так как Rank A < Rank C то данная СЛАУ не имеет решения!")
+
+#%%
+print("Изменим систему: A = [[1, 2, 3], [4, 10, 6], [7, 8, 9]]")
+A = np.array([[1, 2, 3], [4, 10, 6], [7, 8, 9]])
+B = np.array([[12, 2, 1]])
+C = np.concatenate((A,B.T), axis=1)
+print("A:\n",A)
+print("C:\n",C)
+print("Rank A = ", np.linalg.matrix_rank(A, 0.0001))
+print("Rank C = ", np.linalg.matrix_rank(C, 0.0001))
+print("Так как Rank A = Rank С и = числу переменных то система имеет единственное решение!")
+X = np.linalg.solve(A, B[0])
+print("Решение:\n", X)
+
+#%% [markdown]
+# #### 4 - Решение  методом LU разложения ( методм Гаусса )
+
+#%%
+A = np.array([ [1, 2, 3], [2, 16, 21], [4, 28, 73] ])
+B = np.array([12, 2, 1])
+P, L, U = ln.lu(A)
+print("det A = ", np.linalg.det(A))
+print("P = ", P)
+print("L = ", L)
+print("U = ", U)
+
+print("L*Y = B")
+Y = np.linalg.solve(L, B)
+print("Y = ", Y)
+
+print("U*X = Y")
+X = np.linalg.solve(U, Y)
+print("X = ", X)
 
 
